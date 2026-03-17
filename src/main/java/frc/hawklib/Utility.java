da1e2d04c82fa620a0ae2299fdb97e7d2d03961e
techno-thor <joejeakle@gmail.com>
2026-03-17 17:14:28 -0400

    Network Timeout updated

--- src/main/java/frc/hawklib/Utility.java
+++ src/main/java/frc/hawklib/Utility.java
@@ -6,11 +6,14 @@ import edu.wpi.first.wpilibj.Timer;
 public class Utility {
     private static Timer tmrNetworkTimeout = new Timer();
     public static void waitForNetworkTableConnection() {
-        tmrNetworkTimeout.restart();
-        while(!NetworkTableInstance.getDefault().isConnected()){
-            if(tmrNetworkTimeout.get() > 15.0){
-                Logger.printError("Network Tables took too long to connect. Dashboard data might not work correctly.");
-                break;
+        if(!tmrNetworkTimeout.isRunning()) {
+            tmrNetworkTimeout.restart();
+            Logger.printMessage("Waiting for NetworkTables to connect...");
+            while(!NetworkTableInstance.getDefault().isConnected()) {
+                if(tmrNetworkTimeout.get() > 15.0) {
+                    Logger.printError("Network Tables took too long to connect. Dashboard data might not work correctly.");
+                    break;
+                }
             }
         }
     }
